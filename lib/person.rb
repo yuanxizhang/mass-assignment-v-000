44e4fb69776ebc25428258d928a9224e74fbbf53
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-20 22:43:23 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(a)
-    a.each {|key, value| self.send(#{key=}, value) }
+    a.each {|key, value| self.send(("#{key}="), value) }
   end
   
 end
